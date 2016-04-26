a8ec6a746da315f62c51c248c11be481a9f5d317
eliranbi@gmail.com <eliranbi@gmail.com>
2016-04-26 13:44:44 -0400

    update lab10

--- snippets/lab10.js
+++ snippets/lab10.js
@@ -28,15 +28,15 @@
 /* UserLoginSecurityCheck.java */
 
 /* Basic implmentation */        
-package com.ibm;
+	package com.ibm;
 
-import com.ibm.mfp.server.registration.external.model.AuthenticatedUser;
-import com.ibm.mfp.security.checks.base.UserAuthenticationSecurityCheck;
+	import com.ibm.mfp.server.registration.external.model.AuthenticatedUser;
+	import com.ibm.mfp.security.checks.base.UserAuthenticationSecurityCheck;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class UserLogin extends UserAuthenticationSecurityCheck {
+	import java.util.HashMap;
+	import java.util.Map;
+	
+	public class UserLoginSecurityCheck extends UserAuthenticationSecurityCheck {
 
     @Override
     protected AuthenticatedUser createUser() {
@@ -52,7 +52,7 @@ public class UserLogin extends UserAuthenticationSecurityCheck {
     protected Map<String, Object> createChallenge() {
         return null;
     }
-}
+	}
 
 
 /* create the challange */
@@ -84,7 +84,9 @@ protected Map<String, Object> createChallenge() {
     }        
  
 /* Creating the AuthenticatedUser object */
-private String userId, displayName;
+ private String userId, displayName;
+ private String errorMsg;
+
 
 
 /* override the createUser */ 
@@ -92,3 +94,76 @@ private String userId, displayName;
 protected AuthenticatedUser createUser() {
     return new AuthenticatedUser(userId, displayName, this.getName());
 } 
+
+
+/*****  Complete Class ****/
+
+/*
+ * IBM Confidential OCO Source Materials
+ *
+ * 5725-I43 Copyright IBM Corp. 2006, 2015
+ *
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets, irrespective of what has
+ * been deposited with the U.S. Copyright Office.
+ */
+package com.ibm;
+
+import com.ibm.mfp.server.registration.external.model.AuthenticatedUser;
+import com.ibm.mfp.security.checks.base.UserAuthenticationSecurityCheck;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Sample implementation of username/password security check that succeeds if username and password are identical.
+ */
+public class UserLoginSecurityCheck extends UserAuthenticationSecurityCheck {
+    private String userId, displayName;
+    private String errorMsg;
+
+    @Override
+    protected AuthenticatedUser createUser() {
+        return new AuthenticatedUser(userId, displayName, this.getName());
+    }
+
+    /**
+     * This method is called by the base class UserAuthenticationSecurityCheck when an authorization
+     * request is made that requests authorization for this security check or a scope which contains this security check
+     * @param credentials
+     * @return true if the credentials are valid, false otherwise
+     */
+    @Override
+    protected boolean validateCredentials(Map<String, Object> credentials) {
+        if(credentials!=null && credentials.containsKey("username") && credentials.containsKey("password")){
+            String username = credentials.get("username").toString();
+            String password = credentials.get("password").toString();
+            if(username.equals(password)) {
+                userId = username;
+                displayName = username;
+                return true;
+            }
+            else {
+                errorMsg = "Wrong Credentials";
+            }
+        }
+        else{
+            errorMsg = "Credentials not set properly";
+        }
+        return false;
+    }
+
+    /**
+     *
+     * This method is describes the challenge JSON that gets sent to the client during the authorization process
+     * This is called by the base class UserAuthenticationSecurityCheck when validateCredentials returns false and
+     * the number of remaining attempts is > 0
+     * @return the challenge object
+     */
+    @Override
+    protected Map<String, Object> createChallenge() {
+        Map<String, Object> challenge = new HashMap();
+        challenge.put("errorMsg",errorMsg);
+        challenge.put("remainingAttempts",getRemainingAttempts());
+        return challenge;
+    }
+}
